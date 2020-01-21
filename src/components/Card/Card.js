a7fc5e58e4209005f908cbb4acfc5467006f40f3
huyguainnt <huyguainnt@gmail.com>
2020-01-22 06:45:09 +0700

    feat: done Card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Card.scss'
+import ReactCardFlip from 'react-card-flip';
+import { CardsContext } from '../../store/context';
 
 const Card = props => {
-  const { onClick, type, show } = props
+  const { onClick, type, show, completed } = props;
+  const { state, dispatch } = useContext(CardsContext);
+  const { level } = state;
   return (
-    <div className="Card ant-col ant-col-6" onClick={onClick}>
-      <div className={"card-child " + (show ? 'show' : '')}>
-        {
-          show && <span>{type}</span>
-        }
-      </div>
+    <div className={"Card ant-col ant-col-" + (+level === 3 ? "4" : "6")} onClick={onClick} 
+      style={{visibility: completed ? 'hidden' : 'visible' }}>
+      <ReactCardFlip isFlipped={show} flipDirection="horizontal">
+        <div className="card-child">
+        </div>
+        <div className="card-child show">
+          <span>{type}</span>
+        </div>
+      </ReactCardFlip>
     </div>
   )
 }
