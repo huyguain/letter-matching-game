e54c4ac88f6b421cd089c49af3e00a064704da2a
huyguainnt <huyguainnt@gmail.com>
2020-01-22 06:46:18 +0700

    feat: done codding Option WonLose component

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Options.scss'
 import { Select } from 'antd';
 import { Button } from 'antd';
-
-function handleChange(value) {
-  console.log(`selected ${value}`);
-}
+import { CardsContext } from '../../store/context';
 
 const Options = props => {
   const { Option } = Select;
+  const { state, dispatch } = useContext(CardsContext);
+  const { level, remainning } = state;
+  const handleChange = value => {
+    dispatch({ type: 'SET_LEVEL', payload: value });
+    dispatch({ type: 'RESET_CARDS', payload: +value === 3 ? 18 : 8 });
+  }
+  const onStartClick = _ => {
+    dispatch({ type: 'SET_REMAINNING', payload: true })
+  }
   return (
     <div className="Options">
-      <Select defaultValue="0" style={{ width: 110 }} onChange={handleChange}>
+      <Select defaultValue={level} value={level} style={{ width: 110 }} onChange={handleChange} disabled={remainning}>
         <Option value="0">Select level</Option>
         <Option value="1">Easy</Option>
         <Option value="2">Normal</Option>
         <Option value="3">Hard</Option>
       </Select>
-      <Button className="margin-left" type="primary">Start</Button>
+      <Button className="margin-left" type="primary" disabled={!+level || remainning} onClick={onStartClick}>Start</Button>
     </div>
   )
 }
--- src/components/WonLose/WonLose.js
+++ src/components/WonLose/WonLose.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import './WonLose.scss'
+import React, { useContext } from 'react';
+import './WonLose.scss';
+import { CardsContext } from '../../store/context';
 
 const WonLose = props => {
+  const { state, dispatch } = useContext(CardsContext);
+  const { won, lose } = state;
   return (
     <div className="WonLose margin-top">
       <div className="won ant-row">
         <div className="ant-col ant-col-2">Won: </div>
-        <div className="ant-col ant-col-4">0</div>
+        <div className="ant-col ant-col-4">{won}</div>
       </div>
       <div className="lose ant-row">
-        <div className="ant-col ant-col-2">Won: </div>
-        <div className="ant-col ant-col-4">0</div>
+        <div className="ant-col ant-col-2">Lose: </div>
+        <div className="ant-col ant-col-4">{lose}</div>
       </div>
     </div>
   )
