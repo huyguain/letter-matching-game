d61f28130b306cb156069c4a7ffeedd75ce25820
huyguainnt <huyguainnt@gmail.com>
2020-01-22 09:52:53 +0700

    Updates

---
+++ precache-manifest.b8456c8b5b74530e2a5b8009e896a95f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a69832084984a56ea46dad157d92f37",
+    "url": "/letter-matching-game/index.html"
+  },
+  {
+    "revision": "11a7c2ce36bbf48cee69",
+    "url": "/letter-matching-game/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "3e69dc121b9dbdec662e",
+    "url": "/letter-matching-game/static/css/main.6a638273.chunk.css"
+  },
+  {
+    "revision": "11a7c2ce36bbf48cee69",
+    "url": "/letter-matching-game/static/js/2.040cecac.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/letter-matching-game/static/js/2.040cecac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e69dc121b9dbdec662e",
+    "url": "/letter-matching-game/static/js/main.1956ee9e.chunk.js"
+  },
+  {
+    "revision": "21ffba3e6dab030df1cd",
+    "url": "/letter-matching-game/static/js/runtime-main.02b6b005.js"
+  }
+]);
